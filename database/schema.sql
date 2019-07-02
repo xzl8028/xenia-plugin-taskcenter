7876aea62be6950bf5eb926fd19690f2ea8528ce
cchen86 <chenchen415@gmail.com>
2019-07-02 20:41:26 +0800

    Tasks table schema upgrade

--- database/schema.sql
+++ database/schema.sql
@@ -1,17 +1,18 @@
 DROP TABLE IF EXISTS Tasks CASCADE;
 CREATE TABLE Tasks (
   TaskId SERIAL PRIMARY KEY,
-  CreateAt TIMESTAMP DEFAULT current_timestamp,
---   confirm_at TIMESTAMP DEFAULT current_timestamp,
---   timestamp TIMESTAMP DEFAULT current_timestamp,
+  CreateAt BIGINT(20),
+  DueAt BIGINT(20),
+  ConfirmAt BIGINT(20),
+  FinishAt BIGINT(20),
   SendDept TEXT,
   ReceiveDept TEXT,
   RoomId INT,
   TaskType TEXT,
   Note TEXT,
-  Status INT DEFAULT 0 -- 0 in progress, 1 complete
+  Status INT-- DEFAULT 0 -- 0 in progress, 1 complete
 )CHARACTER SET = utf8;
 
-INSERT INTO Tasks (SendDept, ReceiveDept, RoomId, TaskType, Note, Status) VALUES ("前厅部", "客房部", 503, "运送矿泉水", "evan", 1);
-INSERT INTO Tasks (SendDept, ReceiveDept, RoomId, TaskType, Note, Status) VALUES ("前厅部", "工程部", 101, "维修空调", "gree", 2);
-INSERT INTO Tasks (SendDept, ReceiveDept, RoomId, TaskType, Note, Status) VALUES ("前厅部", "餐饮部", 907, "订餐", "steak", 3);
+INSERT INTO Tasks (CreateAt, DueAt, ConfirmAt, FinishAt, SendDept, ReceiveDept, RoomId, TaskType, Note, Status) VALUES (100, 500, 200, 300, "前厅部", "客房部", 503, "运送矿泉水", "evan", 1);
+INSERT INTO Tasks (CreateAt, DueAt, ConfirmAt, FinishAt, SendDept, ReceiveDept, RoomId, TaskType, Note, Status) VALUES (100, 500, 200, 300, "前厅部", "工程部", 101, "维修空调", "gree", 2);
+INSERT INTO Tasks (CreateAt, DueAt, ConfirmAt, FinishAt, SendDept, ReceiveDept, RoomId, TaskType, Note, Status) VALUES (100, 500, 200, 300, "前厅部", "餐饮部", 907, "订餐", "steak", 3);
